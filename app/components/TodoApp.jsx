0a80c1e95f1a50e5c7610f4759ada2525c5ba60f
Sergey <sergavo@gmail.com>
2017-01-05 14:46:10 +0300

    Refactor TodoList, Todo, and AddTodo to work with redux

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,8 +2,8 @@ var React = require('react');
 var uuid = require('uuid');
 var moment = require('moment');
 
-var TodoList = require('TodoList');
-var AddTodo = require('AddTodo');
+import TodoList from 'TodoList';
+import AddTodo from 'AddTodo';
 var Search = require('Search');
 var TodoAPI = require('TodoAPI');
 
@@ -32,18 +32,6 @@ var TodoApp = React.createClass({
         }
       ]
     });
-  },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-
-      return todo;
-    });
-
-    this.setState({todos: updatedTodos});
   },
   handleSearch: function (showCompleted, searchText) {
     this.setState({
