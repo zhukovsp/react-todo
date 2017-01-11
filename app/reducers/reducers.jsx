d60b9d4bde8e352a358af610a62009d5ec2c30a1
Sergey <sergavo@gmail.com>
2017-01-11 14:29:00 +0300

    Add auth  reducer and actions

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -47,8 +47,15 @@ export var todosReducer = (state = [], action) => {
   }
 };
 
-export var toggleReducer = (id, action) => {
+export var authReducer = (state = {}, action) => {
   switch (action.type) {
-
+    case 'LOGIN':
+      return {
+        uid: action.uid
+      };
+    case 'LOGOUT':
+      return {};
+    default:
+      return state;
   }
-};
+}
